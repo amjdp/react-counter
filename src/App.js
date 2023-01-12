5c2c0860f448d53e3efd77d2cf87fa765859cd51
amjdp <athiramj123@gmail.com>
2023-01-12 15:44:22 +0530

    react counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // Counter is a state initialized to 0
+  const [counter, setCounter] = useState(0);
+
+  // Function is called everytime increment button is clicked
+  const handleClick1 = () => {
+    // Counter state is incremented
+    setCounter(counter + 1);
+  };
+
+  // Function is called everytime decrement button is clicked
+  const handleClick2 = () => {
+    // Counter state is decremented
+    setCounter(counter - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Counter App
+      <div className="counter">{counter}</div>
+      <div className="buttons">
+        <button className="btn" onClick={handleClick1}>
+          Increment
+        </button>
+        <button className="btn" onClick={handleClick2}>
+          Decrement
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
